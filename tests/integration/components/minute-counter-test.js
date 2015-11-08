103b42fe05fed0f4f850892e10b5035a5f9db0b5
Tom Netzband <tomnez@gmail.com>
2015-11-08 14:31:07 -0500

    create minute counter component

---
+++ tests/integration/components/minute-counter-test.js
@@ -0,0 +1,26 @@
+// import { moduleForComponent, test } from 'ember-qunit';
+// import hbs from 'htmlbars-inline-precompile';
+
+// moduleForComponent('minute-counter', 'Integration | Component | minute counter', {
+//   integration: true
+// });
+
+// test('it renders', function(assert) {
+//   assert.expect(2);
+
+//   // Set any properties with this.set('myProperty', 'value');
+//   // Handle any actions with this.on('myAction', function(val) { ... });
+
+//   this.render(hbs`{{minute-counter}}`);
+
+//   assert.equal(this.$().text().trim(), '');
+
+//   // Template block usage:
+//   this.render(hbs`
+//     {{#minute-counter}}
+//       template block text
+//     {{/minute-counter}}
+//   `);
+
+//   assert.equal(this.$().text().trim(), 'template block text');
+// });
