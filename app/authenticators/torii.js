036653d3649ae829f8061048856c83c86412aa16
Tom Netzband <tomnez@gmail.com>
2015-11-24 22:21:49 -0500

    clean up torii authenticator

--- app/authenticators/torii.js
+++ app/authenticators/torii.js
@@ -1,27 +1,32 @@
 import Ember from 'ember';
 import Torii from 'ember-simple-auth/authenticators/torii';
 import ENV from '../config/environment';
+import raw from 'ic-ajax';
 
 const { RSVP } = Ember;
 const { service } = Ember.inject;
 
 export default Torii.extend({
-  torii: service(),
+  torii: service('torii'),
 
   serverTokenEndpoint: `${ENV.apiHost}token`,
 
   authenticate() {
     return new RSVP.Promise((resolve, reject) => {
       this._super(...arguments).then((data) => {
-        Ember.$.ajax({
-          type: 'POST',
+        raw({
           url: this.get('serverTokenEndpoint'),
-          data: { token: data.authorizationCode },
+          type: 'POST',
+          dataType: 'json',
+          data: { 'token': data.authorizationCode }
         }).then((response) => {
-          resolve(response);
-        }).fail((error) => {
-          console.log(error.message);
-        });
+          resolve({
+            // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+            access_token: response.access_token,
+            // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+            provider: data.provider
+          });
+        }, reject);
       }, reject);
     });
   }
