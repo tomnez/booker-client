243976faa29fe63b39c163cb91ad72773727f5f6
Tom Netzband <tomnez@gmail.com>
2015-11-27 17:59:13 -0500

    fix failing app route test

--- tests/unit/routes/application-test.js
+++ tests/unit/routes/application-test.js
@@ -3,6 +3,7 @@ import { moduleFor, test } from 'ember-qunit';
 moduleFor('route:application', 'Unit | Route | application', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
+  needs: ['service:session'] // bugfix for https://github.com/simplabs/ember-simple-auth/issues/635
 });
 
 test('it exists', function(assert) {
