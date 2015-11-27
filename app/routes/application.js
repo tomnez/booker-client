5ef0c50dbf703a63534616e0731f1847829c3877
Tom Netzband <tomnez@gmail.com>
2015-11-27 13:42:27 -0500

    add sign in page at index route, create log in/out buttons

--- app/routes/application.js
+++ app/routes/application.js
@@ -7,36 +7,30 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
   session: service(),
   torii: service(),
 
-  beforeModel() {
-    if (!this.get('session.isAuthenticated')) {
-      this.createSession();
-    }
-  },
+  actions: {
+    createSession() {
+      let session = this.get('session');
+      let providerName = 'google-oauth2';
 
-  createSession() {
-    let session = this.get('session');
-    let providerName = 'google-oauth2';
+      this.get('torii').open(providerName).then((googleAuth) => {
 
-    this.get('torii').open(providerName).then((googleAuth) => {
+        let googleToken = googleAuth.authorizationCode;
 
-      let googleToken = googleAuth.authorizationCode;
-
-      session.authenticate('authenticator:torii', providerName, {
-        password: googleToken
-      }).then(() => {
-        console.log('token stored in session.data.authenticated');
-        this.replaceWith('resources');
+        session.authenticate('authenticator:torii', providerName, {
+          password: googleToken
+        }).then(() => {
+          console.log('token stored in session.data.authenticated');
+          this.replaceWith('resources');
+        }, (error) => {
+          console.error(error);
+        });
       }, (error) => {
         console.error(error);
       });
-    }, (error) => {
-      console.error(error);
-    });
-  },
+    },
 
-  actions: {
     invalidateSession() {
       this.get('session').invalidate();
-    }
+    },
   }
 });
