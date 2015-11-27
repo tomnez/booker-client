4731e19a4518ecfb3b45841c0f54fade60d19cf3
Tom Netzband <tomnez@gmail.com>
2015-11-26 21:52:36 -0500

    move authentication into app route beforeModel

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,5 +4,39 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 const { service } = Ember.inject;
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
-  session: service()
+  session: service(),
+  torii: service(),
+
+  beforeModel() {
+    if (!this.get('session.isAuthenticated')) {
+      this.createSession();
+    }
+  },
+
+  createSession() {
+    let session = this.get('session');
+    let providerName = 'google-oauth2';
+
+    this.get('torii').open(providerName).then((googleAuth) => {
+
+      let googleToken = googleAuth.authorizationCode;
+
+      session.authenticate('authenticator:torii', providerName, {
+        password: googleToken
+      }).then(() => {
+        console.log('token stored in session.data.authenticated');
+        this.replaceWith('resources');
+      }, (error) => {
+        console.error(error);
+      });
+    }, (error) => {
+      console.error(error);
+    });
+  },
+
+  actions: {
+    invalidateSession() {
+      this.get('session').invalidate();
+    }
+  }
 });
