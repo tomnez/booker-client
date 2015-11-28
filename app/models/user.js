bd2f0fc3d8457d3914b7f271e6c8181f08f4e40c
Tom Netzband <tomnez@gmail.com>
2015-11-27 22:32:09 -0500

    add user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,3 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend();
+export default DS.Model.extend({
+  email: DS.attr('string'),
+  resources: DS.hasMany('resource', { async: true })
+});
