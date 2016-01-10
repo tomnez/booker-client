e6697d9db88a0e4ae7c13b8b3b3d5a193e4e8d0b
Tom Netzband <tomnez@gmail.com>
2016-01-10 17:05:10 -0500

    get user and resource listing for /resources route

--- app/routes/resources.js
+++ app/routes/resources.js
@@ -1,11 +1,39 @@
 import Ember from 'ember';
+import ENV from '../config/environment';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+const { service } = Ember.inject;
+const { Promise } = Ember.RSVP;
+
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  session: service(),
+
   model(params) {
     let user = this.store.peekAll('user').get('firstObject');
-    // LEFT OFF HERE: if no user, make a user route to get the /me profile and THEN get resources.
-    // move /me request into own method to be shared by /token and /user.
-    return user.get('resources');
+
+    if (!user) {
+      return this.getUser().then((id) => {
+        let user = this.store.peekRecord('user', id);
+        return user.get('resources');
+      });
+    } else {
+      return user.get('resources');
+    }
+  },
+
+  getUser() {
+    return new Promise((resolve, reject) => {
+      Ember.$.ajax({
+        url: `${ENV.apiHost}/me`,
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+          'Authorization': `Bearer ${this.get('session').get('data.authenticated.access_token')}`
+        }
+      }).then((response) => {
+        this.get('store').push(response.user);
+        resolve(response.user.id);
+      }, reject);
+    });
   }
 });
