24a752f250d1aab5daf685a973f78cb91ce02a27
Tom Netzband <tomnez@gmail.com>
2016-01-24 15:18:00 -0500

    upgrade cli, htmlbars-inline-precomp, babel to fix test errors

--- tests/integration/components/minute-counter-test.js
+++ tests/integration/components/minute-counter-test.js
@@ -1,26 +1,17 @@
-// import { moduleForComponent, test } from 'ember-qunit';
-// import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
 
-// moduleForComponent('minute-counter', 'Integration | Component | minute counter', {
-//   integration: true
-// });
+moduleForComponent('minute-counter', 'Integration | Component | minute counter', {
+  integration: true
+});
 
-// test('it renders', function(assert) {
-//   assert.expect(2);
+test('it renders', function(assert) {
+  assert.expect(1);
 
-//   // Set any properties with this.set('myProperty', 'value');
-//   // Handle any actions with this.on('myAction', function(val) { ... });
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
 
-//   this.render(hbs`{{minute-counter}}`);
+  this.render(hbs`{{minute-counter}}`);
 
-//   assert.equal(this.$().text().trim(), '');
-
-//   // Template block usage:
-//   this.render(hbs`
-//     {{#minute-counter}}
-//       template block text
-//     {{/minute-counter}}
-//   `);
-
-//   assert.equal(this.$().text().trim(), 'template block text');
-// });
+  assert.equal(this.$('.counter-btn-container').length, 3, 'All plus, minus, and minute buttons all render.');
+});
