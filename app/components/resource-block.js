76e6ddcaea4a977140fbe32629619f3099f13237
Tom Netzband <tomnez@gmail.com>
2016-02-21 15:55:14 -0500

    get resource event details

--- app/components/resource-block.js
+++ app/components/resource-block.js
@@ -1,13 +1,20 @@
 import Ember from 'ember';
 import moment from 'moment';
+import ENV from '../config/environment';
+import raw from 'ic-ajax';
+
+const { service } = Ember.inject;
 
 export default Ember.Component.extend({
   tagName: 'li',
   classNames: ['resource-block-item'],
   classNameBindings: ['isBooked:is-booked'],
-
+  details: null,
   showDetails: false,
+  loadingDetails: false,
   panel: 'info', // "info" or "counter"
+
+  session: service(),
 
   didInsertElement() {
     let swipeNode = this.$().get(0);
@@ -50,6 +57,7 @@ export default Ember.Component.extend({
 
     this.set('timeNote', difference);
     this.set('freeBusyMessage', message);
+    this.set('disableDetails', !schedule.length);
   },
 
   resourceSwiping(e) {
@@ -104,7 +112,35 @@ export default Ember.Component.extend({
 
   actions: {
     toggleDetails() {
-      this.toggleProperty('showDetails');
+      let session;
+
+      // details already loaded, toggle only
+      if (this.get('details')) {
+        this.toggleProperty('showDetails');
+        return;
+      }
+
+      // get details for first time
+      // show loading
+      this.set('loadingDetails', true);
+
+      // get/set details
+      session = this.get('session');
+
+      raw({
+        url: `${ENV.apiHost}/resources/${this.get('model.id')}/events`,
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+          Authorization: `Bearer ${session.get('data.authenticated.access_token')}`
+        }
+      }).then((response) => {
+        var resourceDetails = response.resourceEvent;
+
+        this.set('details', resourceDetails);
+        this.set('loadingDetails', false);
+        this.toggleProperty('showDetails');
+      });
     },
 
     bookRoom() {
